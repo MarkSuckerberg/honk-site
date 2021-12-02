c0221f2899f5d0fc2ceaee1f5a99d697237cf41e
Honk <throwaway0000ff@gmail.com>
2021-12-01 23:04:15 -0700

    More Testing

--- Shared/charSheet.cs
+++ Shared/charSheet.cs
@@ -2,13 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
-namespace honk-site.Shared
+namespace supercool
 {
     public class charSheet
     {
         public string name = "Name";
         public string pronouns = "Pronoun";
-        public int powerLevel = "150";
-        public Dictionary<string, int> Stats = new() { { "Strength", 0 }, { "Agility", 0 }, { "Fighting", 0 }, { "Awareness", 0 }, { "Stamina", 0 }, { "Dexterity", 0 }, { "Intellect", 0 }, { "Presence", 0 } };
     }
 }
