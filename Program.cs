b8e6da5ca0570e64be37abb3c9cc1f2f7676d23a
Honk <throwaway0000ff@gmail.com>
2021-12-02 19:24:05 -0700

    Ok there were a few instances left behind, should have got them all now

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using honk_site;
+using honksite;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
